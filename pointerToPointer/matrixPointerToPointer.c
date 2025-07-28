611d35d0fa233dae8d57502e23c9a34f23ff8b5e
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-07-28 06:03:52 +0000

    finished reading two matrices

--- pointerToPointer/matrixPointerToPointer.c
+++ pointerToPointer/matrixPointerToPointer.c
@@ -19,6 +19,7 @@ int main()
     
     puts("Successfully opened the file.");
     
+    //MATRIX 1
     //Reading from the file to create matrix
     int rowsMatrix1, columnsMatrix1;
     fscanf(myFile, "%d %d", &rowsMatrix1, &columnsMatrix1);
@@ -35,6 +36,27 @@ int main()
         }
     }
 
+    //Read in matrix 1
+    for(int i = 0; i < rowsMatrix1; ++i)
+    {
+        for(int j = 0; j < columnsMatrix1; ++j)
+        {
+            fscanf(myFile, "%d", &matrix1[i][j]);
+        }
+    }
+
+    //MATRIX 2
+    int rowsMatrix2, columnsMatrix2;
+    fscanf(myFile, "%d %d", &rowsMatrix2, &columnsMatrix2);
+    //Allocate memory row of pointers for matrix 2
+    int **matrix2 = (double *)malloc(rowsMatrix2 * sizeof(double *));
+    //Allocate memory for each row
+    for (int i = 0; i < rowsMatrix2; ++i)
+    {
+        matrix2[i] = (double *)malloc(columnsMatrix2 * sizeof(double));
+    }
+
+    
     //Close the file
     fclose(myFile);
     return 0;
